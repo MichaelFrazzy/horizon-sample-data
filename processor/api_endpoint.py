3f8e6c641f26fb3ee427207b86c4a330f2ef55f7
Michael Frasinelli <91573967+MichaelFrazzy@users.noreply.github.com>
2024-11-22 16:33:41 -0600

    Update api_endpoint.py
    Updated queries to work with new config.yaml and fixed f string.

--- processor/api_endpoint.py
+++ processor/api_endpoint.py
@@ -10,20 +10,18 @@ with open('config/config.yaml', 'r') as f:
     config = yaml.safe_load(f)
 
 # Set up credentials
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = config['credentials_path']
-
-app = Flask(__name__)
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = config['credentials']['path']
 
 @app.route('/daily-volumes', methods=['GET'])
 def get_daily_volumes():
     client = bigquery.Client()
-    query = """
+    query = f"""
     SELECT 
         DATE(date) as date,
         currency_symbol,
         COUNT(*) as transactions,
         ROUND(SUM(usd_volume), 2) as total_usd_volume
-    FROM `sample-data-processor.marketplace_analytics.daily_metrics`
+    FROM `{config['project']['id']}.marketplace_analytics.daily_metrics`
     GROUP BY date, currency_symbol
     ORDER BY date, currency_symbol
     """
@@ -34,13 +32,13 @@ def get_daily_volumes():
 @app.route('/project-volumes', methods=['GET'])
 def get_project_volumes():
     client = bigquery.Client()
-    query = """
+    query = f"""
     SELECT 
         project_id,
         currency_symbol,
         COUNT(*) as total_transactions,
         ROUND(SUM(usd_volume), 2) as total_project_volume
-    FROM `sample-data-processor.marketplace_analytics.daily_metrics`
+    FROM `{config['project']['id']}.marketplace_analytics.daily_metrics`
     GROUP BY project_id, currency_symbol
     ORDER BY project_id, currency_symbol
     """
@@ -48,11 +46,5 @@ def get_project_volumes():
     results = client.query(query).to_dataframe()
     return jsonify(results.to_dict(orient='records'))
 
-# Open browser to endpoints after server starts
-def open_browser():
-    webbrowser.open('http://127.0.0.1:5000/daily-volumes')
-    webbrowser.open('http://127.0.0.1:5000/project-volumes')
-
 if __name__ == '__main__':
-    Timer(1, open_browser).start()
     app.run(port=5000)
